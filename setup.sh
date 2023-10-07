626260748e4b1a8f47a4b463236b62d5de304ce0
Daniel Wood <gunnerwoody449@gmail.com>
2023-10-07 15:17:02 +0100

    chore: Move backup intos its own function

--- setup.sh
+++ setup.sh
@@ -2,25 +2,35 @@
 
 DOTFILES_DIR="$HOME/.dotfiles"
 
-# List to symlink
-declare -a dotfiles=("zshrc" "vimrc" "gitconfig")
+declare -a dotfiles=(".zshrc" ".vimrc" ".gitconfig")
 
-# Function to create symlinks
-create_symlinks() {
+backup_existing() {
   for file in "${dotfiles[@]}"; do
-    if [ -f "$HOME/.$file" ]; then
-      echo "Backing up existing .$file to .$file.bak"
-      mv "$HOME/.$file" "$HOME/.$file.bak"
+    if [ -f "$HOME/$file" ]; then
+      echo "Backing up existing $file to $file.bak"
+      mv "$HOME/$file" "$HOME/$file.bak"
     fi
-    ln -s "$DOTFILES_DIR/$file" "$HOME/.$file"
-    echo "Symlinked .$file to $DOTFILES_DIR/$file"
   done
 }
 
-main() {
+create_symlinks() {
+  for file in "${dotfiles[@]}"; do
+    echo "Creating symlink for $file"
+    if [ -f "$DOTFILES_DIR/$file" ]; then
+      ln -s "$DOTFILES_DIR/$file" "$HOME/$file"
+      echo "Symlinked $file to $DOTFILES_DIR/$file"
+    else
+      echo "Skipping $file - File not found in dotfiles directory."
+    fi
+  done
+}
+
+setup_dotfiles() {
   echo "Setting up dotfiles..."
+  backup_existing
   create_symlinks
   echo "Dotfiles setup complete."
 }
 
-main
+
+setup_dotfiles
